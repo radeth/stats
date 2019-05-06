25823478d9314a67241485b955fcc2bd657018d2
Radosław Moczarski <rmoczarski@KARTGIS.LAN>
2019-05-06 12:49:38 +0200

    init commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import chartData from './../fixtures/chartData'
+
+class App extends React.Component {
+  constructor(){
+    super()
+    console.log(chartData)
+  }
+  render(){
+    return(
+      <div>
+        test
+      </div>
+    )
+  }
+}
+export default App;
